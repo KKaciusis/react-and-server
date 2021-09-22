b55e3f0da0ac1fcd21a1495a807afef744b716c9
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-22 20:06:54 +0300

    rendered todo's array from dummy serv

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Todo from './Todo';
+
+function App() {
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(()=> {
+        console.log('start');
+
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+
+        .then(function (response) {
+            setTodos(response.data);
+        })
+    }, []);
+
+    return (<div className="todo-container">
+        {todos.map((todo)=>(<Todo key={todo.id} data={todo}></Todo>))}
+    </div>)
+}
+export default App;
